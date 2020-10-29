13820f04a735e5b9a8c1dfe904c2b1aef80fb062
TCY168 <s10727128@gm.cyut.edu.tw>
2020-10-29 18:56:39 +0800

    change file

--- playgame/level1-1/level1-1.php
+++ playgame/level1-1/level1-1.php
@@ -5,31 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>資安遊戲闖關網站</title>
-    <script src="./source.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 </head>
 <body a link="blue" vlink="red">
-    <div class="instructions">
-        <h1 style="text-align: center;">LEVEL1-1</h1>
-        <div id="menubar" style="text-align: center";> <a href="../playgame.html" class="menu_exit">退出</a> </div>
-    </div>
-    <hr>
+    <from action="" method="GET">
+        <div class="instructions">
+            <h1 style="text-align: center;">LEVEL1-1</h1>
+            <div id="menubar" style="text-align: center";> <a href="../playgame.html" class="menu_exit">退出</a> </div>
+        </div>
+        <hr>
 
-    <div style="text-align: center;">
-        <a id="next_btn" style="display: none;" href="" >下一關</a> 
-    </div>
-    <br>
+        <div style="text-align: center;">
+            <a id="next_btn" style="display: none;" href="" >下一關</a> 
+        </div>
+        <br>
 
-    <div style="text-align: center;">
-        <p><b>Please injection alert() and pass this level!</b></p>
-        <?php
-            ini_set("display_errors", 0);
-            $str = $_GET["name"];
-            echo "<h2 align=center>Welcome".$str."</h2>";
-        ?>
-    </div>
+        <div style="text-align: center;">
+            <p><b>Please injection alert() and pass this level!</b></p>
+            <?php
+                ini_set("display_errors", 0);
+                $str = $_GET["name"];
+                echo "<h2 align=center>Welcome".$str."</h2>";
+            ?>
+        </div>
+    </from>
+
 
 
 </body>
--- playgame/level1/win.js
+++
@@ -1,8 +0,0 @@
-var originalAlert = window.alert;
-/* Override window.alert*/
-window.alert = function(x){
-   originalAlert("Congratuation! You sucessed!\n" + x + "\nYou can go to next part.");
-   document.getElementById("next_btn").style.display = "block";
-   $( "#next_btn" ).removeClass("next_no").addClass("next_yes");
-   $( "#next_btn" ).attr("href","/playgame/level1-2.html");
-}
