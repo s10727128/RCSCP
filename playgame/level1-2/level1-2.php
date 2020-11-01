15867bb088cc9f40451cbb859a302f77307c470c
TCY168 <s10727128@gm.cyut.edu.tw>
2020-11-01 22:39:59 +0800

    forgot to use echo next goal: 1. let hint button press and show the hint by number

--- playgame/level1-2/level1-2.php
+++ playgame/level1-2/level1-2.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>資安遊戲闖關網站</title>
-    <script src="../source.js"></script>
+    <script src="../source2.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
---
+++ playgame/source1.js
@@ -0,0 +1,8 @@
+var originalAlert = window.alert;
+/* Override window.alert*/
+window.alert = function(){
+   originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
+   document.getElementById("next_btn").style.display = "block";
+   //$( "#next_btn" ).removeClass("next_no").addClass("next_yes");
+   $( "#next_btn" ).attr("href","/playgame/level1-2/level1-2.php");
+}
