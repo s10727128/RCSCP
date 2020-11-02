d7f3244d9be4917d5a0315a358ae6102c0b01c5e
TCY168 <s10727128@gm.cyut.edu.tw>
2020-11-02 14:30:23 +0800

    level1-1 to level1-2 okay

--- playgame/source1.js
+++ playgame/source1.js
@@ -3,6 +3,6 @@ var originalAlert = window.alert;
 window.alert = function(){
    originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
    document.getElementById("next_btn").style.display = "block";
-   $( "#next_btn" ).removeClass("next_no").addClass("next_yes");
-   $( "#next_btn" ).attr("href","/js-hackgame/playgame/level1-2/level1-2.php");
+   //$( "#next_btn" ).removeClass("next_no").addClass("next_yes");
+   $( "#next_btn" ).attr("href","../level1-2/level1-2.php");
 }
