49d38aec2ec70c8a8576e3b00c46a3dab5ccda27
TCY168 <s10727128@gm.cyut.edu.tw>
2020-11-04 20:32:31 +0800

    keep try it

--- playgame/source3.js
+++ playgame/source3.js
@@ -1,5 +1,9 @@
 var originalAlert = window.alert;
 
+function Myfuction(){
+   
+}
+
 window.alert = function(){
    originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
    document.getElementById("next_btn").style.display = "block";
