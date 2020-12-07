5a88c31a463b237bf18f7a1fb594c6eb7a54eb48
TCY168 <s10727128@gm.cyut.edu.tw>
2020-12-07 20:02:45 +0800

    revise href in level1

--- playgame/source1.js
+++ playgame/source1.js
@@ -3,7 +3,7 @@ var originalAlert = window.alert;
 window.alert = function(){
    originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
    document.getElementById("next_btn").style.display = "block";
-   $( "#next_btn" ).attr("href","../level1-2/level1-2.php?name=test");
+   $( "#next_btn" ).attr("href","../playgame/level1-2.php?name=test");
 }
 
 $("#writeup").attr("href","../level1-1/level1-1writeup.txt");
--- playgame/source3.js
+++ playgame/source3.js
@@ -7,5 +7,5 @@ function Myfuction(){
 window.alert = function(){
    originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
    document.getElementById("next_btn").style.display = "block";
-   $( "#next_btn" ).attr("href","../level1-3/level1-3.php?name=test");
+   $( "#next_btn" ).attr("href","../level1-3.php?name=test");
 }
