3361dbca43ed4f7edac87b0ddb527dd774351b00
TCY168 <s10727128@gm.cyut.edu.tw>
2020-12-12 14:32:05 +0800

    Data can not storge

--- playgame/source3.js
+++ playgame/source3.js
@@ -9,3 +9,12 @@ window.alert = function(){
    document.getElementById("next_btn").style.display = "block";
    $( "#next_btn" ).attr("href","../level1-3.php?name=test");
 }
+
+
+function saveStorge(id){
+   var data = document.getElementsById(id).value;
+   var time = new Data().getTime();
+   localStorage.setItem(time,data);
+   alert("123");
+
+}
