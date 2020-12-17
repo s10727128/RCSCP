96206cbbdc1ff708775c153921ebb27850bceab1
TCY168 <s10727128@gm.cyut.edu.tw>
2020-12-17 20:15:10 +0800

    sendbtn function get something wrong

---
+++ playgame/chat.js
@@ -0,0 +1,18 @@
+var originalAlert = window.alert;
+//var messagelogs = document.getElementById("messagelogs");
+var talkbox = document.getElementsByName("talk_box").values;
+var sendbtn = document.getElementById("chat-form-button");
+
+window.alert = function(){
+   originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
+   document.getElementById("next_btn").style.display = "block";
+   $( "#next_btn" ).attr("href","../playgame/level1-2.php?name=test");
+}
+
+function sendFunction(){
+    var str = "";
+    str = '<div class="message"><div class="user-photo"><img src="../elephant.jpeg"></div>' + '<p class="message_chat">' + talkbox + '</p></div>';
+    messagelogs.innerHTML = messagelogs.innerHTML + str;
+}
+
+
