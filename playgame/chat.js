5fcca65fabd4e609933cdc8f40763681fde05504
TCY168 <s10727128@gm.cyut.edu.tw>
2020-12-20 13:44:34 +0800

    get wrong

--- playgame/chat.js
+++ playgame/chat.js
@@ -1,8 +1,11 @@
 var originalAlert = window.alert;
 //var messagelogs = document.getElementById("messagelogs");
 //var talkbox = document.getElementById("talkbox").innerText;
-const talkboxElement = document.getElementById("talkbox");
-const talkbox = talkboxElement.value;
+//var talkboxElement = document.getElementById("talkbox");
+//var talkbox = talkboxElement.;
+const form = document.forms['chat-form'];
+const textbox = form.elements.talkbox.value;
+
 
 window.alert = function(){
    originalAlert("Congratuation! You sucessed!\n" + "\nYou can go to next part.");
@@ -16,5 +19,6 @@ function sendFunction(){
     messagelogs.innerHTML = messagelogs.innerHTML + str;
     document.getElementById("talkbox").value="";
 }
+
 
 
