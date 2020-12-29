3d7414fb7f4e87fc443508fbd8f505fa9bbe4646
TCY168 <s10727128@gm.cyut.edu.tw>
2020-12-29 15:24:13 +0800

    modalcontent test

--- playgame/level1-1.php
+++ playgame/level1-1.php
@@ -47,6 +47,8 @@
             </div>
     </form>
 
+
+    modal
     
     <script>
         let x = 0;
---
+++ playgame/modalcontent.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <link rel="stylesheet" a href ="playgame1.css">
+    <link rel="shortcut icon" href="/bird.jpg">
+    <title>Modal Content</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="source1.js"></script>
+  </head>
+
+  <body>
+    <body a link="blue" vlink="red">
+        <form action="" method="GET">
+            <div class="instructions">
+                <h1>LEVEL1-1</h1>
+                <div id="menubar" > <a href="gamepoint/level1index.html" class="menu_exit">EXIT</a> </div>
+            </div>
+            <hr>
+    
+            <div>
+                <a id="next_btn" style="display: none;" href="" >Next</a> 
+            </div>
+            <br>    
+            <div">
+                <p><b>這個關卡是一個很經典的XSS漏洞，屬於Reflected XSS (反射型)，是由網頁後端直接嵌入由前端使用者所傳送過來的內容造成的。</b></p>
+                <p><b>請嘗試在網頁注入一個 alert() ，即可即可過關!</b></p>
+            </div>
+    
+            <div>
+                <?php
+                    ini_set("display_errors", 0);
+                    $ans = $_GET["name"];
+                    echo "Your answer is: $ans";
+                ?>
+            </div>
+    
+            <br>
+
+
+            <!-- <button type="button" id="hintList" class="button">Hint</button>
+                <div class="word2" style="list-style: none; ">
+                
+                    <li id="hint1"></li>
+                    <li id="hint2"></li>
+                    <li id="hint3"></li>
+                    <li id="hint4"></li>
+                </div>
+        </form>
+    
+        <script>
+            let x = 0;
+            $("#hintList").click(function(){
+                x ++;
+                switch(x){
+                    case 1:
+                        $("#hint1").text("1. 執行一個<script>");
+                        break;
+    
+                    case 2:
+                        $("#hint2").text("2. 在<script>裡加入alert()");
+                        break;
+    
+                    case 3:
+                        $("#hint3").text("3. 不知道輸入在哪嗎?仔細觀察網址");
+                        break;
+    
+                    case 4:
+                    $("#hint4").text("4. 將neam=test，改成name\=\<script\>alert\(\)\<\/script\>");
+                    break;
+    
+                    default:
+                        break;
+                }
+            });
+        </script> -->
+
+
+
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#modelId">
+      Hint
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                    <div class="modal-header">
+                            <h5 class="modal-title">Hint</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                        </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <p>1.執行一個< script></p>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script>
+        $('#exampleModal').on('show.bs.modal', event => {
+            var button = $(event.relatedTarget);
+            var modal = $(this);
+            // Use above variables to manipulate the DOM
+            
+        });
+    </script>
+
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
