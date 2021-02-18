ab7ee5b3fdc6931d32f680f8605d740734dccee9
GXDDXD <a84968827@gmail.com>
2021-02-18 11:15:40 +0800

    Access ,AddAcount and DeleteAccount  's css ok

--- Teamproject/register/account/ModifyAccount_Access.php
+++ Teamproject/register/account/ModifyAccount_Access.php
@@ -11,6 +11,20 @@ $access=$_POST['Access'];
 $password = MD5($pwd);
 $sql = "UPDATE user SET  Password='$password',Score='$score',Access='$access' WHERE Username='$name'";
 $result=mysqli_query($connect,$sql);
-echo "修改成功,5秒後自動返回主介面";//成功輸出修改成功
-echo '<meta http-equiv=REFRESH CONTENT=5;url=access.php>';
+echo'<div class="forok">';
+echo "修改成功,3秒後自動返回主介面";//成功輸出修改成功
+echo '</div>';
+echo '<meta http-equiv=REFRESH CONTENT=3;url=access.php>';
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ModifyAccount_Access.css">
+</head>
+<body>
+    
+</body>
+</html>
