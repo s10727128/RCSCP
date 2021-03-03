77e647defacc5e07e34b87b25504cbb04e868f3b
GXDDXD <a84968827@gmail.com>
2021-03-03 15:17:56 +0800

    level 3-1 done and css debug

--- playgame/MISC/MISC1/MISC1.php
+++ playgame/MISC/MISC1/MISC1.php
@@ -88,29 +88,7 @@
 
     ?>
     
-    <!-- Modal -->
-    <div class="hint">
-        <div class="modal fade" id="modelId" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Hint</h5>
-                        <!-- <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button> -->
-                    </div>
-                    <div class="modal-body">
-                        <div class="container-fluid">
-                            <h1><B>摩斯密碼</B></h1>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>  
+
       <script>
         $('#exampleModal').on('show.bs.modal', event => {
             var button = $(event.relatedTarget);
