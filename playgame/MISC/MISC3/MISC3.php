b7b960cdc5aa14d35cc839906ac5db3c310bfbbb
GXDDXD <a84968827@gmail.com>
2021-03-18 20:30:06 +0800

    index bar

--- playgame/MISC/MISC3/MISC3.php
+++ playgame/MISC/MISC3/MISC3.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" a href="../../CSS/playgame5.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="source.js"></script>
+    <!-- <script src="source.js"></script> -->
 </head>
 
 <body a link="blue" vlink="red">
