89fd18126e0088a9fea8f686215facf2977d9e21
TCY168 <s10727128@gm.cyut.edu.tw>
2021-03-18 20:31:49 +0800

    csrffile test

--- ALLTESTFILE/test1.js
+++ ALLTESTFILE/test1.js
@@ -1,8 +1,24 @@
-$("#button").click(function Add(){
-    var id = $("#id").val();
-    var name = $("#name").val();
-    var fraction = $("#fraction").val();
+// $("#button").click(function Add(){
+//     var id = $("#id").val();
+//     var name = $("#name").val();
+//     var fraction = $("#fraction").val();
 
-    var str = '<tr><th>ID</th><th>姓名</th><th>積分</th></tr>';
-    $("#data").after(str);
-})
+//     var str = '<tr><th>ID</th><th>姓名</th><th>積分</th></tr>';
+//     $("#data").after(str);
+// })
+
+function Add(){
+    var id = document.getElementById("id").value;
+    var name = document.getElementById("name").value;
+    var fraction = document.getElementById("fraction").value;
+
+    var str = "";
+    str = '<div class="data"><tr><th>' + id + '</th><th>' + name + '</th><th>' + fraction + '</th></tr></div>';
+    datatable.innerHTML = datatable.innerHTML + str;
+
+    document.getElementById("id").value = "";
+    document.getElementById("name").value = "";
+    document.getElementById("fraction").value = "";
+
+
+}
---
+++ ALLTESTFILE/try.js
@@ -0,0 +1,11 @@
+// $(document).ready();
+
+// $("#btn").click(function press(){
+//     var str;
+//     $("#answer").after("<tr><th>ID</th><th>姓名</th><th>積分</th></tr>");
+// })
+
+function Add(){
+    document.getElementById("input").nodeValue;
+    document.getElementById("answer1").innerHTML = "123";
+}
