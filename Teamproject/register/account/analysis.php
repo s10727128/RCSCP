d142ce165188062623463ff765c8d316591946f4
GXDDXD <a84968827@gmail.com>
2021-03-25 19:38:51 +0800

    analysis.css

--- Teamproject/register/account/analysis.php
+++ Teamproject/register/account/analysis.php
@@ -597,7 +597,7 @@ include("../../header.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/test.css">
+    <link rel="stylesheet" href="analysis.css">
     <title>Document</title>
 </head>
 <body>
