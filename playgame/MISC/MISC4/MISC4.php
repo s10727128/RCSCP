40c081a37c4091b0bbfd5c1a03b82590fb6e27a6
TCY168 <s10727128@gm.cyut.edu.tw>
2021-05-03 20:07:20 +0800

    MISC all done, but time is later than real time

--- playgame/MISC/MISC4/MISC4.php
+++ playgame/MISC/MISC4/MISC4.php
@@ -46,21 +46,128 @@
                 <a href="../../../downloadfile/stego.zip" download><b>Download</b></a>
             </form>
 
-            <?php
-            ini_set("display_errors", 0);
-            $ans = $_GET["input"];
-            if ($ans == "UpassAllMisc666!") {
-                echo '<script>alert("Congratuation! You sucessed!")</script>';
-            }
-            echo '<form action=MISC4.php =GET>
-            <input name=input value"' . $ans . '"/>
-            <input type=submit name=submit value="送出"/>
-            </form><br>';
-            ?>
+            <div>
+                <form action="MISC4.php" method="post">
+                    <b>通關密碼</b>
+                    <input type="varchar" name="Key">
+                    <input type="submit" name="KeySubmit" value="驗證">
+                </form>
+            </div>
         </div>
+
+            <!-- 更新程式  開始-->
+
+    <?php
+        #防止warning出現
+        ini_set("display_errors", 0);
+        #記住開始的時間點
+        if ($_SESSION["MISC4_time"] == null)//每個關卡的SESSION名字都要改
+        {
+            $_SESSION["MISC4_time"] = time();
+        }
+        else if($_SESSION["MISC4_time"]!=null){
+            echo '開始時間'.(date('Y-m-d H:i:s',$_SESSION["MISC4_time"]+7*3600));//顯示開始時間點,可不加
+        }
+        #通關驗證
+        if(isset($_POST["KeySubmit"]))
+        {
+        $Key = $_POST["Key"];
+        #判斷是不是正確答案
+        if($Key == "UpassAllMisc666!")
+        {   
+        $enddate=time();//讀取完成時間
+        $resultendtate = mysqli_query($connect,"UPDATE game SET  MISC_4open='$enddate' WHERE Username='$name'");
+        $totaltime=($enddate-$_SESSION["MISC4_time"]);
+        // echo '<br>'."結束時間:".(date('Y-m-d H:i:s',$enddate+7*3600));
+        // echo '<br>'."總共花費:".($enddate-$_SESSION["time"])."秒";//顯示結束時間點,可不加
+                #搜尋user裡的玩家的分數
+                $resultscore=mysqli_query($connect,"SELECT Score FROM user WHERE Username='$name'");
+                if(mysqli_num_rows($resultscore) > 0)
+                {
+                while($row = mysqli_fetch_assoc($resultscore))
+                {
+                    $score=$row["Score"];
+                }
+                }
+                #確認玩家是否已經有分數  
+                $resultcheck=mysqli_query($connect,"SELECT MISC_4 FROM game WHERE Username='$name'");
+                if(mysqli_num_rows($resultcheck) > 0)
+                {
+                while($row = mysqli_fetch_assoc($resultcheck))
+                {
+                    $check=$row["MISC_4"];
+                }
+                }
+                #判斷是否已經有分數
+                if($check>0)
+                {
+                $score+=0;//如果有分數,則不再另外給分
+                }
+                #根據通關時間給分
+                else
+                {
+                if($totaltime<=300){//時間少於5分鐘
+                    $score+=200;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=200    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                    }
+                else if($totaltime>300&&$totaltime<=600){//時間在5分鐘~10分鐘間
+                    $score+=180;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=180    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                    }
+                else if($totaltime>600&&$totaltime<=900){//時間在10分鐘~15分鐘間
+                    $score+=160;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=160    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                    }
+                else if($totaltime>900&&$totaltime<=1200){//時間在15分鐘~20分鐘間
+                    $score+=140;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=140    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                }
+                else if($totaltime>1200&&$totaltime<=1800){//時間在20分鐘~30分鐘間
+                    $score+=120;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=120    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                }
+                else if($totaltime>1800&&$totaltime<=2700){//時間在30分鐘~45分鐘間
+                    $score+=100;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=100    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                }
+                else if($totaltime>2700&&$totaltime<=3600){//時間在45分鐘~1小時間
+                    $score+=75;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=75    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                }
+                else if($totaltime>3600){//時間大於1小時
+                    $score=50;
+                    $result=mysqli_query($connect,"UPDATE game SET  MISC_4=50    WHERE Username='$name'");
+                    $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
+                }
+                }        
+                echo '<div class="pass"><br>'."恭喜通關~<br>";
+                //echo '<a href="../MISC4/MISC4.php">前往下一關</a><br>';
+                echo '<a href="../../../Teamproject/html/gameset.php">返回主頁</a><br></div>';
+        }
+        
+        #不是正確答案的情況
+        else
+        {   
+            echo '<div class="fail">';
+            echo '<br>'."通關密碼不正確,請繼續加油!<br>";
+            //echo '<a href="MISC1.php">返回關卡</a><br>';
+            echo '</div>';
+        }
+        }
+    ?>
+
+    <!-- 結束 -->
+
         <?php
-    include("MISC4.html");
- ?>
+            include("MISC4.html");
+        ?>
         <!-- <div style="text-align: center;">
             <p>請幫我拿解開檔案，謝謝!</p>
         </div>
