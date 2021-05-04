968f11cfd0d8072a6f5637a71bee2a57c2379865
TCY168 <s10727128@gm.cyut.edu.tw>
2021-05-04 17:09:41 +0800

    MISC5 keep write hint

--- playgame/MISC/MISC5/MISC5.php
+++ playgame/MISC/MISC5/MISC5.php
@@ -7,12 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php
     include_once("../../../Teamproject/header.php");
-
     ?>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <!-- <script src="source.js"></script> -->
     <link rel="stylesheet" a href="../../CSS/playgame5.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 
 <body a link="blue" vlink="red">
@@ -25,7 +23,7 @@
                         <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
                     </svg>
                 </a>
-                <b>MISC1</b>
+                <b>MISC5</b>
             </h1>
 
         </div>
@@ -35,36 +33,36 @@
             <a id="next_btn" style="display: none;" href="">Next</a>
         </div>
         <br>
+
         
         <div>
             <p><b>--找到密碼過關--</b></p><br>
         </div>
 
         <div style="text-align: center;">
-            <form action="../../../downloadfile/MISC1.txt" method="GET">
+            <form action="../../../downloadfile/MISC5.txt" method="GET">
                 下載點:
-                <a href="../../../downloadfile/MISC1.txt" download><b>Download</b></a>
+                <a href="../../../downloadfile/MISC5.txt" download><b>Download</b></a>
             </form>
 
             <div>
-                <form action="MISC1.php" method="post">
+                <form action="MISC5.php" method="post">
                     <b>通關密碼</b>
                     <input type="varchar" name="Key">
                     <input type="submit" name="KeySubmit" value="驗證">
                 </form>
             </div>
         </div>
-        <!-- 更新程式  開始-->
 
         <?php
         #防止warning出現
         ini_set("display_errors", 0);
         #記住開始的時間點
-        if ($_SESSION["MISC1_time"] == null)//每個關卡的SESSION名字都要改
+        if ($_SESSION["MISC5_time"] == null)//每個關卡的SESSION名字都要改
         {
-            $_SESSION["MISC1_time"] = time();
+            $_SESSION["MISC5_time"] = time();
         }
-        else if($_SESSION["MISC1_time"]!=null){
+        else if($_SESSION["MISC5_time"]!=null){
             //echo '開始時間'.(date('Y-m-d H:i:s',$_SESSION["MISC1_time"]+7*3600));//顯示開始時間點,可不加
         }
         #通關驗證
@@ -72,11 +70,11 @@
         {
             $Key = $_POST["Key"];
             #判斷是不是正確答案
-            if($Key == "thisyearis2021")
+            if($Key == "thisisCaesarcipher")
             {   
             $enddate=time();//讀取完成時間
-            $resultendtate = mysqli_query($connect,"UPDATE game SET  MISC_1open='$enddate' WHERE Username='$name'");
-            $totaltime=($enddate-$_SESSION["MISC1_time"]);
+            $resultendtate = mysqli_query($connect,"UPDATE game SET  MISC_5open='$enddate' WHERE Username='$name'");
+            $totaltime=($enddate-$_SESSION["MISC5_time"]);
             // echo '<br>'."結束時間:".(date('Y-m-d H:i:s',$enddate+7*3600));
             // echo '<br>'."總共花費:".($enddate-$_SESSION["time"])."秒";//顯示結束時間點,可不加
                     #搜尋user裡的玩家的分數
@@ -89,12 +87,12 @@
                     }
                     }
                     #確認玩家是否已經有分數  
-                    $resultcheck=mysqli_query($connect,"SELECT MISC_1 FROM game WHERE Username='$name'");
+                    $resultcheck=mysqli_query($connect,"SELECT MISC_5 FROM game WHERE Username='$name'");
                     if(mysqli_num_rows($resultcheck) > 0)
                     {
                     while($row = mysqli_fetch_assoc($resultcheck))
                     {
-                        $check=$row["MISC_1"];
+                        $check=$row["MISC_5"];
                     }
                     }
                     #判斷是否已經有分數
@@ -107,42 +105,42 @@
                     {
                     if($totaltime<=300){//時間少於5分鐘
                         $score+=200;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=200    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=200    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                         }
                     else if($totaltime>300&&$totaltime<=600){//時間在5分鐘~10分鐘間
                         $score+=180;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=180    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=180    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                         }
                     else if($totaltime>600&&$totaltime<=900){//時間在10分鐘~15分鐘間
                         $score+=160;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=160    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=160    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                         }
                     else if($totaltime>900&&$totaltime<=1200){//時間在15分鐘~20分鐘間
                         $score+=140;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=140    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=140    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                     }
                     else if($totaltime>1200&&$totaltime<=1800){//時間在20分鐘~30分鐘間
                         $score+=120;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=120    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=120    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                     }
                     else if($totaltime>1800&&$totaltime<=2700){//時間在30分鐘~45分鐘間
                         $score+=100;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=100    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=100    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                     }
                     else if($totaltime>2700&&$totaltime<=3600){//時間在45分鐘~1小時間
                         $score+=75;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=75    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=75    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                     }
                     else if($totaltime>3600){//時間大於1小時
                         $score=50;
-                        $result=mysqli_query($connect,"UPDATE game SET  MISC_1=50    WHERE Username='$name'");
+                        $result=mysqli_query($connect,"UPDATE game SET  MISC_5=50    WHERE Username='$name'");
                         $result=mysqli_query($connect,"UPDATE user SET  score=$score WHERE Username='$name'");
                     }
                     }        
@@ -162,51 +160,19 @@
         }
         ?>
 
-        <!-- 結束 -->
-
-        <!-- <div style="text-align: center;">
-        <button type="button" id="hintList">Hint</button>
-        <div style="list-style: none; ">
-            <li id="hint1"><b></b></li>
-        </div>
-    </div>
-
-    <script>
-        let j = 0;
-        $("#hintList").click(function() {
-            j++;
-            switch (j) {
-                case 1:
-                    $("#hint1").text("1. 摩斯密碼");
-                    break;
-            }
-        });
-    </script> -->
-        <!-- Button trigger modal -->
-        <!-- <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modelId">
-        <b>提示</b>
-    </button> -->
-
-
-
-        <!--Hint -->
         <?php
-        include("MISC1.html");
-
+            include("MISC5.html");
         ?>
 
 
-        <script>
-            $('#exampleModal').on('show.bs.modal', event => {
-                var button = $(event.relatedTarget);
-                var modal = $(this);
-                // Use above variables to manipulate the DOM
+    <br><br>
+    <br><br>
+    <br><br>
+    <br><br>
+    <br><br>
+    <br><br>
+    <br>
 
-            });
-        </script>
-
-    </div>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 
 
 
@@ -224,11 +190,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-
-
-
-
 </body>
+
 </html>
