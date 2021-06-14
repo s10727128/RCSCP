602881ac2c1efa533cf92e0c27b505cebc3ef677
popo3267 <73634087+popo3267@users.noreply.github.com>
2021-06-14 19:44:26 +0800

    delete phishing

--- Teamproject/register/account/ModifyScore.php
+++ Teamproject/register/account/ModifyScore.php
@@ -24,12 +24,6 @@ if($access!=1){
   <b>修改XSS3分數:</b>
   <input type="varchar" name="XSS3" id="Score">
   <br>
-  <b>修改Phishing1分數:</b>
-  <input type="varchar" name="Phishing1" id="Score">
-  <br>
-  <b>修改Phishing2分數:</b>
-  <input type="varchar" name="Phishing2" id="Score">
-  <br>
   <b>修改SQL1分數:</b>
   <input type="varchar" name="SQL1" id="Score">
   <br>
@@ -73,8 +67,6 @@ $name=mysqli_real_escape_string($connect,$_POST['Username']);
 $XSS1=mysqli_real_escape_string($connect,$_POST['XSS1']);
 $XSS2=mysqli_real_escape_string($connect,$_POST['XSS2']);
 $XSS3=mysqli_real_escape_string($connect,$_POST['XSS3']);
-$Phishing1=mysqli_real_escape_string($connect,$_POST['Phishing1']);
-$Phishing2=mysqli_real_escape_string($connect,$_POST['Phishing2']);
 $SQL1=mysqli_real_escape_string($connect,$_POST['SQL1']);
 $SQL2=mysqli_real_escape_string($connect,$_POST['SQL2']);
 $CSRF1=mysqli_real_escape_string($connect,$_POST['CSRF1']);
@@ -85,11 +77,11 @@ $MISC3=mysqli_real_escape_string($connect,$_POST['MISC3']);
 $MISC4=mysqli_real_escape_string($connect,$_POST['MISC4']);
 $MISC5=mysqli_real_escape_string($connect,$_POST['MISC5']);
 $MISC6=mysqli_real_escape_string($connect,$_POST['MISC6']);
-$sql = "UPDATE game SET  XSS_1='$XSS1',XSS_2='$XSS2',XSS_3='$XSS3',Phishing_1='$Phishing1',Phishing_2='$Phishing2'
-,SQL_1='$SQL1',SQL_2='$SQL2',CSRF_1='$CSRF1',CSRF_2='$CSRF2',MISC_1='$MISC1',MISC_2='$MISC2'
+$sql = "UPDATE game SET  XSS_1='$XSS1',XSS_2='$XSS2',XSS_3='$XSS3',SQL_1='$SQL1',SQL_2='$SQL2'
+,CSRF_1='$CSRF1',CSRF_2='$CSRF2',MISC_1='$MISC1',MISC_2='$MISC2'
 ,MISC_3='$MISC3',MISC_4='$MISC4',MISC_5='$MISC5',MISC_6='$MISC6' WHERE Username='$name'";
 $result=mysqli_query($connect,$sql);
-$sum=$XSS1+$XSS2+$XSS3+$Phishing1+$Phishing2+$SQL1+$SQL2+$CSRF1+$CSRF2+$MISC1+$MISC2+$MISC3+$MISC4+$MISC5+$MISC6;
+$sum=$XSS1+$XSS2+$XSS3+$SQL1+$SQL2+$CSRF1+$CSRF2+$MISC1+$MISC2+$MISC3+$MISC4+$MISC5+$MISC6;
 $sql="UPDATE user SET Score='$sum' WHERE Username='$name'";
 $result=mysqli_query($connect,$sql);
 echo'<div class="forok">';
