150cd862c6513a6a12c5b7289ba2908861a27f17
TCY168 <s10727128@gm.cyut.edu.tw>
2021-07-07 16:00:01 +0800

    test

---
+++ ALLTESTFILE/testphpjs.php
@@ -0,0 +1,34 @@
+<?php
+    //  Just Keep going......
+    //  Do not turn back.......
+    //  Until the day you find the 01100001 01101110 01110011 01110111 01100101 01110010.......
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  01010111 01101111 01110010 01101001 01101110 01100111 00001010...
+    //  \5{Dy_R-uM=HG5XH$~xKpX8cu}M?CRSy|R7vwV!XF#(6]XN(@}q{&4M<SA\<'8y+3
+    //  cxuSFYMF\r2bz_2'+U5T;ofj@@@@@@@@@@@@@@@@@@3s>*?8R\'xaf*}P<2rkc(`Wq5,^Qq[G%Ep9exfZK-Kg2gfeb4ew
+    //  ZF[DG69b{U8u[8`;&VTX\@@@@@@@@@@@@@@@@@@@@@@@@R7\-:fQ8m{+vEHWKcT+gX3*h,U[s"_ZmdmvsK%G4kGKesvdrfgs
+    //   E|}S=@S{x:!;Te'={}@@@W\e[fX@@@@@@@@@@.>mKk~@@@@})b)}.]8nkqZ<;`Ge"6H}S]m"v}S<rPB-Ssergv
+    //  fty7q\kU#=4Kf4TQ@`=@@@7'c7h{@@@@@@@@@@"&Du{'@@@@&m+EYR7tBEc"Z]{ZhQ+?x3HFzBUt8]+CyDsegfvsefv
+    //  !f{UGA=$ub;/Mx5wTup@@@"64ZHh@@@@@@@@@@Q,K-W4@@@@[37,xrSWuurRsUNE$#B]7zv]U[?p%w~-w3gasergv
+    //  dPvSNfWdf8?8gUb(ts`G@@@@@@@@@@@@@@@@@@@@@@@@@@@DYkaX8W_hM${mb]}qkRChQ.5nTh{Y\_"f``^Wu+#eHEemefvsefgv
+    //  STF!P/Aq(:#[/KQhX]Z9_Y@@@@@@@@@@sdf@@@@@@@@@@@76w.h6!q9^G,Chqu5{|/?Q5rEK`zZApWh)5U'}pQzuCfsdgw64un
+    //  K?]/pN8's8cHw;U^Q8Ps.sfC7@@@@@@@DFE@@@@@@@Xx=bSh#Z$cmR6_8VFB4uP#n4vrb$N;sdZV{t#>?<Yservs
+    //  u@*b~HaNH4[=\"EdudB]VY?)x74[@@@@@@@@@@@\Bq\#G]a[e'pUPM&6k`snra\EHgEHF'}P}]brsdgvsegvw
+    //  v)u/Rk)(aR:^[_[dPw]63CRmyzVPf@@/@@n3@@@<,XUsNpcn#QSfPARe#'D3c>Mcc*vuCpapa2kq2sd@
+    //  |]*Ak(:Vbs"hT%Uf.qw%_<+sNUM#R@@P@@3F@@@,z&{%:;$rq&AQ$$TC|2"b`|:w{+FyYw5UZb/8dV4QYS
+    //  /`#EF^Q\4eKN:fCkNhx^Wf.Y82hQ"@@@@@@@@@@nDh8NcB|>S}eVYb~md>V9ek.YD?f/,]'Z@4ySQH2v%F~-
+    //  &Mgh57B"}-({A8WS;PF6X(G~.dq3n7SzSzU'_/r4$C*(aB&/EY&_`_ds]5W8{9tvY#P%'
+    //  YAdqBB<ngRs~\H4D2p~"8u_=mKv3.tx->2cSy4!N,4G%NC>9@U(B#mV{"eSS$,s;T/'<2vB6zSh(3
+
+
+
+
+
+
+
+
+?>
